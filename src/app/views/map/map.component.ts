e81780ef68ab99d2c7f9575a658ec63c863c17da
Valery Semenenko <gearmobile@gmail.com>
2019-12-17 15:19:42 +0300

    feat: init project

---
+++ src/app/views/map/map.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'll-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.scss'],
+})
+export class MapComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
